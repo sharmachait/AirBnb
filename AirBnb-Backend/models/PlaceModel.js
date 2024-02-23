ad7bdcfeac1553a2c96de8a7fa200c316c2b9365
29!@#vansthaliA <chait8126@gmail.com>
2024-02-23 12:28:14 +0530

    add accomodation form

---
+++ AirBnb-Backend/models/PlaceModel.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const PlaceSchema = new mongoose.Schema({
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    },
+    title: String,
+    address: String,
+    photos: [String],
+    description: String,
+    perks: [String],
+    checkIn: Number,
+    checkOut: Number,
+    maxGuests: Number
+});
+
+const PlaceModel = mongoose.model('Place', PlaceSchema);
+
+module.exports = PlaceModel;
